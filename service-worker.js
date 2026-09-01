a6b79a328069db64029a7a0d3ada97b6e04a0643
tdariusdavid-png <t.dariusdavid@gmail.com>
2026-09-01 21:51:33 +0200

    Force refresh for serial number fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agriutil-erp-v5';
+const CACHE_NAME = 'agriutil-erp-v6';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
