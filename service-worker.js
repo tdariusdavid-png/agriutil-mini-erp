e2f3fb0b690ce0cd32b93f906121acbbba342040
tdariusdavid-png <t.dariusdavid@gmail.com>
2026-09-01 21:42:01 +0200

    Refresh cache for PDF title wrapping

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agriutil-erp-v2';
+const CACHE_NAME = 'agriutil-erp-v3';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
