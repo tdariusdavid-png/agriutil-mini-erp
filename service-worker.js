ab825a19d5cafde539f41747cd2199db772a418d
tdariusdavid-png <t.dariusdavid@gmail.com>
2026-09-01 21:58:46 +0200

    Force cache refresh for native sales icon

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agriutil-erp-v7';
+const CACHE_NAME = 'agriutil-erp-v8';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
