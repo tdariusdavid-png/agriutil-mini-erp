b5355c4d3d47822c947851ae46da07f875ea4ebf
tdariusdavid-png <t.dariusdavid@gmail.com>
2026-09-01 23:23:43 +0200

    Refresh PWA cache for legal registers

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agriutil-erp-v8';
+const CACHE_NAME = 'agriutil-erp-v9';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
