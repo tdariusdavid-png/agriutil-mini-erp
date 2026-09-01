9f36764ed5153cbb48520cfa8618829a15eda44a
tdariusdavid-png <t.dariusdavid@gmail.com>
2026-09-01 21:46:00 +0200

    Refresh cache for client PDF details

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agriutil-erp-v3';
+const CACHE_NAME = 'agriutil-erp-v4';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
