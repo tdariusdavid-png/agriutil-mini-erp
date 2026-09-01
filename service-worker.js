4ebd13672ac57c0b4881d4d3119155a8e6ac8ba2
tdariusdavid-png <t.dariusdavid@gmail.com>
2026-09-01 21:55:24 +0200

    Refresh cache for highlighted sales navigation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agriutil-erp-v6';
+const CACHE_NAME = 'agriutil-erp-v7';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
